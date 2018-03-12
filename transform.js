55a861550a5a372fc422b3a2632a2772a7f94e8e
suoyue <1255915270@qq.com>
2018-03-12 14:16:04 +0800

    bug

--- transform.js
+++ transform.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
+const marked = require('marked');
 const baseUrl = 'https://hitsuoyue.github.io/gegezhan/';
 let list = [];
 
@@ -8,8 +9,9 @@ fs.readdir('./list', function (error, files) {
     files.forEach( file=>{
         let p = path.join('./list', file);
         let markdown = fs.readFileSync(p).toString();
+        let html = marked(markdown);
         let template = fs.readFileSync('./template.html').toString();
-        let result = template.replace('%content%', markdown);
+        let result = template.replace('%content%', html);
         fs.writeFileSync(file+'.html', result);
     })
 });
@@ -18,20 +20,21 @@ fs.readdir('./', function (error, files) {
     files.forEach(file=>{
         if(file.substring(file.length-7, file.length) === 'md.html'){
             list.push(file);
-        };
+        }
     })
     modifyList();
 });
 
 function modifyList() {
     let content = fs.readFileSync('./index.html');
-    console.log('11111')
     $ = cheerio.load(content);
     let dom = $('#content');
     dom.empty();
     let ul = `<ul class="container"></ul>`;
     dom.append(ul);
     let container = $('.container');
+
+
     list.forEach((item,index)=>{
         let url = `${baseUrl}${item}`;
         let title = item.substring(0, item.length-8);
